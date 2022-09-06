180d81baae914c42ae8410ccbec0d5a770a9ad53
204G1A0533 <112563839+204G1A0533@users.noreply.github.com>
2022-09-06 12:08:28 +0530

    Gnapika Bai M. Created FCFS.c

---
+++ Experiment-1/FCFS.c
@@ -0,0 +1,6 @@
+/*name the Candidate: Gnapika Bai M.
+   Roll No: 204G1A0533
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 29-08-2022
+   Date of Execution: 29-08-2022
+*/
